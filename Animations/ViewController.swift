145164f09574c417babe63ff104ebf445f3a8e43
Eirik Bo Eidsvig <eirikboeidsvig@Eiriks-MacBook-Pro.local>
2016-03-18 17:34:25 +0100

    Added project

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pictureView: UIImageView!
+    
+    // Create a counter outside any function
+    var counter = 1
+    
+    // Make a timer of type NSTimer to be refered to inside and outside funtions in the viewcontroller class
+    var timer = NSTimer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set up a timer method to be run by the viewDidLoad that is set up to call/run a funtion every chosen time intervall. The function in question is the updating image function
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: "updateImage", userInfo: nil, repeats: true)
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateImage() {
+        
+        // Set the loop to make a value for the counter one higher than the previous value if that value was less than 14
+        if counter < 14 {
+            
+            counter = counter + 1
+        // if not, just set value of counter to 1
+        } else {
+            
+            counter = 1
+            
+        }
+        // Make a string that corresponds to the filename and set that to the image attribute of the image view
+        pictureView.image = UIImage(named: "girl\(counter).gif")
+        
     }
-
 
 }
 
